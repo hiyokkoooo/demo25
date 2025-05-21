a8e3f4a86912dcb96cc3a8e225ca3f36f96a3b42
hiyokkoooo <85033269+hiyokkoooo@users.noreply.github.com>
2025-05-22 06:29:18 +0700

    Create import.sh

---
+++ import.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+csv_file="/opt/users.csv"
+while IFS=";" read -r firstName lastName role phone ou street zip city country password; do
+	if [ "$firstName" == "First Name" ]; then
+		continue
+	fi
+	username="${firstName,,}.${lastName,,}"
+	samba-tool user add "$username" P@ssw0rd;
+done < "$csv_file"

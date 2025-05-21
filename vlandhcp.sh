b864958706a4cd0c8b8aec9c8c182462018c5b20
hiyokkoooo <85033269+hiyokkoooo@users.noreply.github.com>
2025-05-22 06:30:58 +0700

    Update vlandhcp.sh

--- vlandhcp.sh
+++ vlandhcp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Создание VLAN 100
-mkdir -p /etc/network/ifaces/ens19.100/
-cat > /etc/network/ifaces/ens19.100/options <<EOF
+mkdir -p /etc/net/ifaces/ens19.100/
+cat > /etc/net/ifaces/ens19.100/options <<EOF
 TYPE=vlan
 HOST=ens19
 VID=100
@@ -10,14 +10,14 @@ BOOTPROTO=static
 EOF
 
 # Создание VLAN 200
-mkdir -p /etc/network/ifaces/ens19.200/
-cat > /etc/network/ifaces/ens19.200/options <<EOF
+mkdir -p /etc/net/ifaces/ens19.200/
+cat > /etc/net/ifaces/ens19.200/options <<EOF
 TYPE=vlan
 HOST=ens19
 VID=200
 BOOTPROTO=static
 EOF
-echo "10.1.1.78/28" > /etc/network/ifaces/ens19.200/ipv4address
+echo "10.1.1.78/28" > /etc/net/ifaces/ens19.200/ipv4address
 
 # Перезапуск сети
 systemctl restart network
@@ -38,4 +38,4 @@ EOF
 systemctl enable --now dnsmasq
 systemctl restart dnsmasq
 
-echo "Настройка завершена в ТОЧНОСТИ как указано"
+echo "Настройка завершена"

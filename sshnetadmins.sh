445fe3d8b0a29bb2c0607219399bf3d8b3fcf157
hiyokkoooo <85033269+hiyokkoooo@users.noreply.github.com>
2025-05-24 16:41:26 +0700

    Update sshnetadmins.sh

--- sshnetadmins.sh
+++ sshnetadmins.sh
@@ -14,7 +14,7 @@ echo "net_admin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 # 5. Настройка SSH (порт 2024 и запрет root-логина)
 sed -i 's/#Port 22/Port 2024/' /etc/openssh/sshd_config
-sed -i 's/#PermitRootLogin yes/PermitRootLogin no/' /etc/openssh/sshd_config
+sed -i 's/#PermitRootLogin without-password/PermitRootLogin no/' /etc/openssh/sshd_config
 
 # 6. Перезапуск SSH
 systemctl restart sshd
